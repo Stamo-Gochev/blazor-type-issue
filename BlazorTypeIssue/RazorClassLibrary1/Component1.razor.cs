a7c4e3cb0f2ac5cc551dc3504bd35f57592565c1
Stamo-Gochev <stamo_gochev@yahoo.com>
2021-10-27 10:24:41 +0300

    chore: add net 5.0 apps

---
+++ BlazorTypeIssue/RazorClassLibrary1/Component1.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace RazorClassLibrary1
+{
+    public partial class Component1 : ComponentBase
+    {
+        public string Id { get; set; } = "123";
+    }
+}
